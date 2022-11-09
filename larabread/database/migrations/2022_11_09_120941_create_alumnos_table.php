4c69f5797b4d664e2547c35854db14398b271597
Zet <jonaysepulvedac@gmail.com>
2022-11-09 12:56:56 +0000

    primer paso

---
+++ larabread/database/migrations/2022_11_09_120941_create_alumnos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 20);
+            $table->string('apellidos', 50);
+            $table->string('email', 60)->unique();
+            $table->date('f_nacimiento')->nullable();
+            $table->string('c_postal', 10);
+        });
+    }
+
+    /**Alumno: id, nombre, apellidos, email, f_nacimiento, c_postal 
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumnos');
+    }
+};
---
+++ larabread/database/migrations/2022_11_09_121002_create_recetas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recetas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 20);
+            $table->string('descripcion', 500);
+            $table->double('precio', 10, 2);
+            $table->integer('calorias');
+            $table->date('f_alta')->nullable();
+        });
+    }
+
+    /**Receta: id, nombre, descripcion, precio, calorias, f_alta 
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recetas');
+    }
+};
